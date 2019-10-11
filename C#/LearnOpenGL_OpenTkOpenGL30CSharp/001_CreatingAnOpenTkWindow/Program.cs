50a4111c546202aa946ddb04250deb0af066be6b
Ivan Enzhaev <8observer8@gmail.com>
2019-10-11 17:32:50 +0400

    First Commit

---
+++ C#/LearnOpenGL_OpenTkOpenGL30CSharp/001_CreatingAnOpenTkWindow/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace _001_CreatingAnOpenTkWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new Window())
+            {
+                window.Run();
+            }
+        }
+    }
+}
---
+++ C#/LearnOpenGL_OpenTkOpenGL30CSharp/001_CreatingAnOpenTkWindow/Window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace _001_CreatingAnOpenTkWindow
+{
+    class Window : GameWindow
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(0.1f, 0.3f, 0.2f, 1f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+    }
+}
